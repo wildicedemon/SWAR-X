f5a56b9351fa5b4d65cf7234573fb9b034118d5e
Michel Blankenstein <michel.blankenstein@gmail.com>
2017-02-22 10:40:58 +0100

    # Updated commonLib to Ankulua's github version with keeping modifications

--- lib/commonLib.lua
+++ lib/commonLib.lua
@@ -33,15 +33,26 @@ function regionFinder(target, var)
     if debugAll == true then toast("Found region: "..x..", "..y..", "..w..", "..h) end
     return orgReg
 end
-function autoResize(target, defaultDimension, immersive)
+
+--[[
+-- Determines the size on which images are matched.
+-- ]]
+function autoResize(target, defaultDimension, immersive, region)
+    local oldROI = Settings:getROI();
     local max = 0
     local localX = defaultDimension
+    if (region) then Settings:setROI(region) end
     if (exists(target, 0)) then
+        resumeROI(oldROI)
         return defaultDimension
     end
 
+    usePreviousSnap(true)
+
     setImmersiveMode(not immersive)
     if (exists(target, 0)) then
+        resumeROI(oldROI)
+        usePreviousSnap(false)
         return defaultDimension
     end
 
@@ -72,14 +83,48 @@ function autoResize(target, defaultDimension, immersive)
             max = getLastMatch():getScore()
         end
     end
+
     if (max == 0) then
         Settings:setCompareDimension(true, defaultDimension)
         Settings:setScriptDimension(true, defaultDimension)
+	resumeROI(oldROI)
+        usePreviousSnap(false)
         return -1
     end
 
+    resumeROI(oldROI)
+    usePreviousSnap(false)
     return (localX + 9)
 end
+
+-- ============= Image recognition related =============
+function regionWaitMulti(target, seconds, debug, skipLocation)
+    local timer = Timer()
+    local match
+    while (true) do
+        for i, t in ipairs(target) do
+            if (debug) then t.region:highlight(0.5) end
+            if (i == 1) then usePreviousSnap(false) else usePreviousSnap(true) end
+            if ((t.region and (t.region):exists(t.target, 0)) or
+                    (not t.region and exists(t.target, 0))) then -- check once
+                usePreviousSnap(false)
+                if (t.region) then
+                    match = (t.region):getLastMatch()
+                else
+                    match = getLastMatch
+                end
+                if (debug) then match:highlight(0.5) end
+                return i, t.id, match
+            end
+        end
+        if (skipLocation ~= nil) then click(skipLocation) end
+        if (timer:check() > seconds) then
+            usePreviousSnap(false)
+            return -1, "__none__"
+        end
+    end
+end
+
 function waitMulti(target, seconds, skipLocation)
     local timer = Timer()
     while (true) do
@@ -223,6 +268,8 @@ function resumeROI(oldROI)
         Settings:setROI()
     end
 end
+
+-- ============= UI related ================
 function simpleDialog(title, message)
     dialogInit()
     addTextView(message)
@@ -237,6 +284,41 @@ function detectLanguage(target, list)
     end
     return (getLanguage())
 end
+
+function offOnScreen(second, pinLock, pin)
+    keyevent(26) -- power
+    wait(second)
+    keyevent(82) --unlock
+    if (pinLock) then
+        type(pin) --passcode
+        keyevent(66) -- enter
+    end
+end
+
+-- ============= strings related ================
+function fileExists(name)
+    local f=io.open(name,"r")
+    if f~=nil then io.close(f) return true else return false end
+end
+
+function loadStrings(path)
+    local language = getLanguage()
+    local file = path.."strings."..getLanguage()..".lua";
+    if (fileExists(file)) then
+        dofile(file)
+    else
+        if (fileExists(path.."strings.lua")) then
+            dofile(path.."strings.lua")
+        end
+    end
+end
+
+-- ============= Lua language related =============
+function tableLookup(table, item)
+    for i, t in ipairs(table) do
+        if (t == item) then return i end
+    end
+    return -1
 end
 
 function stageDetect()
@@ -256,4 +338,3 @@ function levelSelect()
 
 
 end
-
