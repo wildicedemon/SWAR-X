03a809a5c496dafab81cec762feb220578c12a7b
Michel Blankenstein <michel.blankenstein@gmail.com>
2017-02-22 13:57:43 +0100

    ## -- GENERAL -- ## # Made some functions to make the code more DRY
    ## -- IMAGES & REGIONS-- ##
    # Changed the location of the images to allow other resolutions to be added more easily
    # Assigned almost all image Patterns to variables to be more flexible with refactoring the scripts and make it more clean
    # Moved all Pattern assignments to a special image.lua and dofile on top of main.lua to load it
    # Moved all Region assignments to a special resolution name based lua file to allow other resolutions to be added more easily
    # Renamed some images to have more consistent names
    # Added a language to some images because they will probably be different when another language is used
    
    ## -- SETTINGS & INTERFACE -- ##
    # Moved the settings and interfaces to the top of the main.lua
    # Added the resolution option to the first dialogShow() because it is used for function autoResize() and determining the image path and compareDimension
    # Added function to use the AppUsableScreenSize()
    
    ## -- TESTING PURPOSES -- ##
    # Added debug toasts to the beginning of all functions to better see what the script is doing
    # Added several debug highlights to better see what the script is doing

---
+++ lib/regions_2560x1440.lua
@@ -0,0 +1,71 @@
+-- ========================
+-- 	Regions for resolution
+-- 		2560 x 1440
+-- ========================
+upperRight = Region(1900, 0, 500, 440)
+right = Region(1284, 500, 960, 560)
+left = Region(300, 500, 960, 560)
+bottomLeft = Region(0, 880, 684, 556)
+buttonRegion = Region(768, 964, 1028, 428)
+rechargeEnergy = Region(400,400,650,550)
+runeSnap = Region(700,0,1150,1440)
+statsSection = Region(800,0,950,200)
+
+--StageList Loop Regions
+battleFigReg = Region(0,1220,225,221) --Battle Fig
+victoryDiamondReg = Region(1800,580,350,300) --Victory Diamond
+worldMapReg = Region(1632,1040,440,440) --World Map
+bigFlashReg = Region(2000,855,250,230) --Big Flash
+arenaBigWingReg = Region(2000,855,250,230) --Big Wing
+playReg =  Region(368,1220,225,221) --play Region
+pauseReg = Region(1050,537,460,187) -- Pause
+arenaResultsReg = Region(320,800,250,250) --Arena Results Page
+bigCancalReg = Region(0,0,2560,1440)
+battleButtonReg = Region(1275,1150,335,290)
+areaMapReg = Region(2225,1150,335,290)
+dungeonStageReg = Region(300,350,800,1050)
+
+yesWordPngReg = Region(798,751,530,263) --yesWordPng
+cancel2Reg = Region(1577,190,300,800) --cancel2.png
+cancelLongReg = Region(1050,150,800,400) --cancelLong.png
+cancelCrossDarkReg = Region(1575,95,305,585) --cancelCrossDark.png
+cancelRefillReg = Region(2031,91,208,219) --cancelRegill.png
+okenReg = Region(1026,949,503,310) --ok.en.png
+
+--Monster Region Regions
+TopMon = Region(492,232,320,320)
+LeftMon = Region(220,376,320,320)
+RightMon = Region(764,376,320,320)
+BottomMon = Region(492,520,320,320)
+NewFodder = Region(0,780,1800,380)
+EndofMonL = Region(1660,780,400,460)
+TopMonEXP = Region(600,890,460,220)
+LeftMonEXP = Region(1100,890,460,220)
+RightMonEXP = Region(1620,890,460,220)
+BottomMonEXP = Region(580,1062,460,220)
+FifthMonEXP = Region(1100,1062,460,220)
+SixthMonEXP = Region(1620,1062,460,220)
+--Arena Monster/Enemy Regions
+eTopMon = Region(1760,224,300,300)
+eLeftMon = Region(1500,370,300,300)
+eBottomMon = Region(1760,512,300,300)
+eRightMon = Region(2020,370,300,300)
+--Arena Battle Regions
+arenaOppReg = Region(1860,145,400,1100)
+EndofArenaL = Region(1600,925,650,325)
+arenaRefreshReg = Region(1675,150,600,225)
+arenaRefreshWaitReg = Region(850,180,840,220)
+arenaRefreshListReg = Region(850,635,850,300)
+arenaDialogReg = Region(0,1011,190,190)
+--Rune Evaluation Regions
+runeStarRegion = Region(755,377,350,325)
+runeStarsRegionD = Region(850,475,150,150) --Debug Rune Star Display
+runeRarityRegion = Region(766,250,1025,300)
+runePrimeRegion = Region(930,430,600,120)
+runeSubRegion = Region(770,625,480,325)
+runeEvalStats = Region(1278,620,500,665)
+--Needs to be implemented.
+runeShopStarRegion = Region(245,298,350,325)
+runeShopRarityRegion = Region(255,237,1025,300)
+runeShopPrimeRegion = Region(419,336,600,120)
+runeShopSubRegion = Region(255,510,480,350)
