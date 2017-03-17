87be907e80eb8a3d5e297bcb6aa7ff2fb18ce300
Michel Blankenstein <michel.blankenstein@gmail.com>
2017-03-17 15:12:21 +0100

    Rearrange code to be prettier

--- lib/regions_2560x1600.lua
+++ lib/regions_2560x1600.lua
@@ -2,90 +2,94 @@
 -- 	Regions for resolution
 -- 		2560 x 1600
 -- ========================
-upperRight = Region(1760, 0, 800, 800)
-right = Region(1320, 600, 950, 560)
-left = Region(300, 600, 950, 560)
-bottomLeft = Region(0, 900, 700, 700)
+upperRight              = Region(1760, 0, 800, 800)
+right                   = Region(1320, 600, 950, 560)
+left                    = Region(300, 600, 950, 560)
+bottomLeft              = Region(0, 900, 700, 700)
 
-buttonRegion = Region(770, 990, 1020, 440)
-rechargeEnergy = Region(125,480,1320,800)
+buttonRegion            = Region(770, 990, 1020, 440)
+rechargeEnergy          = Region(125,480,1320,800)
 
-runeSnap = Region(700,360,1150,1100)
-statsSection = Region(2260,550,300,350)
+runeSnap                = Region(700,360,1150,1100)
+statsSection            = Region(2260,550,300,350)
 
 --StageList Loop Regions
-battleGearWheelReg = Region(0,1380,225,220)
-victoryDiamondReg = Region(1800,670,280,260)
-victoryRaidDamageReg = Region(170,670,370,150)
-worldMapReg = Region(1630,1200,200,200)
-bigFlashReg = Region(2050,1000,200,150)
-arenaBigWingReg = Region(2000,855,250,230)
-playReg =  Region(494,1452,70,90)
-pauseReg = Region(940,610,650,180)
-arenaResultsReg = Region(235,890,250,250)
-bigCancalReg = Region(0,0,2560,1600)
+battleGearWheelReg      = Region(0,1380,225,220)
+victoryDiamondReg       = Region(1800,670,280,260)
+victoryRaidDamageReg    = Region(170,670,370,150)
+worldMapReg             = Region(1630,1200,200,200)
+bigFlashReg             = Region(2050,1000,200,150)
+arenaBigWingReg         = Region(2000,855,250,230)
+playReg                 = Region(494,1452,70,90)
+pauseReg                = Region(940,610,650,180)
+arenaResultsReg         = Region(235,890,250,250)
+bigCancalReg            = Region(0,0,2560,1600)
 
-battleButtonReg = Region(1180,1315,275,255)
-areaMapReg = Region(2300,1350,220,210)
-dungeonStageReg = Region(190,400,855,1140)
+battleButtonReg         = Region(1180,1315,275,255)
+areaMapReg              = Region(2300,1350,220,210)
+dungeonStageReg         = Region(190,400,855,1140)
 
-yesWordPngReg = Region(830,875,398,178)
-okenReg = Region(1026,870,503,440)
+yesWordPngReg           = Region(830,875,398,178)
+okenReg                 = Region(1026,870,503,440)
 
-cancelCrossReg = Region(1575,190,305,800)
-cancel2Reg = Region(1575,190,305,800)
-cancelLongReg = Region(1575,190,305,800)
-cancelRefillReg = Region(1760,0,800,800)
+cancelCrossReg          = Region(1575,190,305,800)
+cancel2Reg              = Region(1575,190,305,800)
+cancelLongReg           = Region(1575,190,305,800)
+cancelRefillReg         = Region(1760,0,800,800)
 
 --Ally Monster Regions
-TopMon = Region(525,300,250,250)
-LeftMon = Region(235,455,250,250)
-RightMon = Region(815,455,250,250)
-BottomMon = Region(525,610,250,250)
+TopMon                  = Region(525,300,250,250)
+LeftMon                 = Region(235,455,250,250)
+RightMon                = Region(815,455,250,250)
+BottomMon               = Region(525,610,250,250)
 
 --Enemy Monster Regions
-eTopMon = Region(1785,300,250,250)
-eLeftMon = Region(1495,455,250,250)
-eRightMon = Region(2075,455,250,250)
-eBottomMon = Region(1785,610,250,250)
+eTopMon                 = Region(1785,300,250,250)
+eLeftMon                = Region(1495,455,250,250)
+eRightMon               = Region(2075,455,250,250)
+eBottomMon              = Region(1785,610,250,250)
 
 --(Fodder) Leveing Monster Regions
-NewFodder = Region(0,850,1800,450)
-EndofMonL = Region(1540,850,430,460)
-monListLeft = Location(120,1090)
-monListRight = Location(1660,1090)
+NewFodder               = Region(0,850,1800,450)
+EndofMonL               = Region(1540,850,430,460)
+monListLeft             = Location(120,1090)
+monListRight            = Location(1660,1090)
 
-TopMonEXP = Region(400,965,520,200)
-LeftMonEXP = Region(970,965,520,200)
-RightMonEXP = Region(1550,965,520,200)
+TopMonEXP               = Region(400,965,520,200)
+LeftMonEXP              = Region(970,965,520,200)
+RightMonEXP             = Region(1550,965,520,200)
 
-BottomMonEXP = Region(400,1150,520,200)
-FifthMonEXP = Region(970,1150,520,200) -- UNUSED
-SixthMonEXP = Region(1550,1150,520,200) -- UNUSED
+BottomMonEXP            = Region(400,1150,520,200)
+FifthMonEXP             = Region(970,1150,520,200) -- UNUSED
+SixthMonEXP             = Region(1550,1150,520,200) -- UNUSED
 
 --Arena Battle Regions
-arenaOppReg = Region(1940,225,400,1100)
-EndofArenaL = Region(1620,1030,650,325)
-arenaRefreshReg = Region(1745,185,600,220)
-arenaRefreshWaitReg = Region(850,200,850,220)
-arenaRefreshListReg = Region(850,760,850,300)
-arenaDialogReg = Region(0,1100,300,300)
+arenaOppReg             = Region(1740,215,400,1200)
+EndofArenaL             = Region(1570,1160,655,325)
+arenaRefreshReg         = Region(1605,205,560,180)
+arenaRefreshWaitReg     = Region(850,200,850,220)
+arenaRefreshListReg     = Region(850,760,850,300)
+arenaDialogReg          = Region(0,1100,300,300)
+arenaSelectNormalReg    = Region(460,320,715,930)
+arenaSelectWorldReg     = Region(1370,320,715,930)
+arenaBattleLogReg       = Region(1450,1315,275,255)
+arenaButtonReg          = Region(1850,530,640,540)
 
 --Rune Evaluation Regions
-runeStarRegion = Region(795,510,190,190)
-runeStarsRegionD = Region(795,510,190,190) --Debug Rune Star Display
-runeTypeAndSlotRegion = Region(800,390,900,100)
-runeRarityRegion = Region(1450, 360, 380, 400)
-runePrimeRegion = Region(980,500,600,120)
-runeSubRegion = Region(770,700,600,340)
-runeEvalStats = Region(1510,680,300,400)
+runeStarRegion          = Region(795,510,190,190)
+runeStarsRegionD        = Region(795,510,190,190) --Debug Rune Star Display
+runeTypeAndSlotRegion   = Region(800,390,900,100)
+runeRarityRegion        = Region(1450, 360, 380, 400)
+runePrimeRegion         = Region(980,500,600,120)
+runeSubRegion           = Region(770,700,600,340)
+runeEvalStats           = Region(1510,680,300,400)
 
 --Needs to be implemented.
-runeShopStarRegion = Region(245,298,350,325) -- UNUSED
-runeShopRarityRegion = Region(255,237,1025,300) -- UNUSED
-runeShopPrimeRegion = Region(419,336,600,120) -- UNUSED
-runeShopSubRegion = Region(255,510,480,350) -- UNUSED
-runeShopStarsRegionD = Region(850,475,150,150) -- UNUSED
-runeShopEvalStats = Region(1330,530,500,665) -- UNUSED
+runeShopStarRegion      = Region(245,298,350,325) -- UNUSED
+runeShopRarityRegion    = Region(255,237,1025,300) -- UNUSED
+runeShopPrimeRegion     = Region(419,336,600,120) -- UNUSED
+runeShopSubRegion       = Region(255,510,480,350) -- UNUSED
+runeShopStarsRegionD    = Region(850,475,150,150) -- UNUSED
+runeShopEvalStats       = Region(1330,530,500,665) -- UNUSED
 
 -- TODO: Gift to all friends function
